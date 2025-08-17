a2975ece90c2afdbb7f2311e5f6be7046142b516
Tree-ro <123820371+Tree-Ro@users.noreply.github.com>
2025-08-17 14:34:21 +0200

    Bump version

--- animeSummary/animeSummary.user.js
+++ animeSummary/animeSummary.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Anime Summary
 // @namespace    http://tampermonkey.net/
-// @version      1.33
+// @version      1.34
 // @description  A kinda customisable userscript querying anilist for summary + cover of the previous rounds anime and then display it.
 // @author       Mooero
 // @match        https://animemusicquiz.com/*
